b54ba94d0dbf049cebe4f619c84d019c3ed3c65a
J. Andres Guerrero <jaguerrero@caixabanktech.com>
2023-10-03 00:35:32 +0200

    First juguerre vial version for q3 ansi rotary

---
+++ keyboards/keychron/q3/ansi_encoder/keymaps/juguerre_vial/rules.mk
@@ -0,0 +1,9 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIALRGB_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+
+VPATH += keyboards/keychron/common
+SRC += keychron_common.c
